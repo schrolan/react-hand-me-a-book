6dfcd7bbcbc3a30200ca7fd115d8dcb6747066e7
schrolan <schrolan000@gmail.com>
2024-04-30 18:16:51 -0500

    I finished the connection in config in server and got the server working in the server folder.

--- server/server.js
+++ server/server.js
@@ -1,39 +1,32 @@
-// const path = require('path')
-// const express = require('express')
-// const connection = require('./config/connection')
-// const { ApolloServer } = require('@apollo/server')
-// const { expressMiddleware } = require('@apollo/server/express4')
-// const typeDefs = require('./schemas/typeDefs')
-// const resolvers = require('./schemas/resolvers')
-// const { authMiddleware } = require('./utils/auth')
+//Path comes with node so no need to install it
+const path = require('path')
+const express = require('express')
+//This will start up the mongo connection
+const connection = require('./config/connection')
 
-// const PORT = process.env.PORT || 3001
-// const app = express()
+//This will use the .env port all caps from heroku or the local host of 3001
+const PORT = process.env.PORT || 3001
+//This is the express app
+const app = express()
 
-// const apolloServer = new ApolloServer({ typeDefs, resolvers })
+//There is differnt ways of sending data on the body of the request. urlencoded is one way of sending data. urlencoded is a middleware.
+//The extended false is one setting that you can add to this middleware
+app.use(express.urlencoded({ extended: false }))
+//This allows us to parse json data
+app.use(express.json())
 
-// app.use(express.urlencoded({ extended: false }))
-// app.use(express.json())
+//This sets the build folder as the static directory
+if (process.env.NODE_ENV === 'production') {
+    //This will set the path to the build folder. .. up then to client folder then to build folder.
+    app.use(express.static(path.join(__dirname, "..", "client", "build")))
+}
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
-// }
+//This is the route for the home route. This serves up the homepage from the compiled react app.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'client', 'index.html'))
+})
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
-// })
-
-// connection.once('open', async () => {
-//     await apolloServer.start()
-//     app.use('/graphql', expressMiddleware(apolloServer, {
-//         context: authMiddleware
-//     }))
-    
-//     app.listen(PORT, () => {
-//         console.log(`Express server listening on http://localhost:${PORT}`)
-//         console.log(`Apollo GrapghQL playground available at http://localhost:${PORT}/graphql`)
-//     })
-// })
-
-
-console.log('Hello world from server in the server folder!')
+//We want to use the connection we imported above. This is an event listener on mongoose and you can listen to an event only one time. Inside we start up the express server.
+connection.once('open', async () => {
+    console.log(`Express server listening on http://localhost:${PORT}`)
+})
