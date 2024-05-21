95cb2b59bedd045ddb6e0d65d63a98be0c553f19
schrolan <schrolan000@gmail.com>
2024-05-20 21:15:56 -0500

    Mutations have been set up

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,7 @@ const { Book, User } = require('../models')
 
 const resolvers = {
     //Use return to return something to your query
+    //These are for reading the data, mutations are for changing the data
     Query: {
         books: async (parent, args, context, info) => {
             return await Book.find()
@@ -14,6 +15,22 @@ const resolvers = {
         },
         user: async (parent, args, context, info) => {
             return await User.findById(args._id)
+        }
+    },
+    Mutation: {
+        addBook: async (parent, args, context, info) => {
+            const book = await Book.create(args)
+            if (args.userId) {
+                await User.findByIdAndUpdate(args.userId, {
+                    $addToSet: {
+                        book: book._id
+                    }
+                })
+            }
+            return book
+        },
+        addUser: async (parent, args, context, info) => {
+            return await User.create(args)
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,6 +20,21 @@ const typeDefs = `
         users: [User]
         user(_id: ID!): User
     }
+
+    type Mutation {
+        addBook(
+            userId: ID!
+            title: String!
+            author_name: [String]
+            cover_i: Int
+            first_publish_year: Int
+        ): Book
+        addUser(
+            username: String!
+            email: String!
+            password: String!
+        ):User
+    }
 `
 
 module.exports = typeDefs
