5fc155198ec4f72c718351fb9d6af863999c803e
schrolan <schrolan000@gmail.com>
2024-07-01 21:20:10 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,26 +1,12 @@
 const mongoose = require('mongoose');
 
 // Retrieve MongoDB URI from environment variables
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://Lance:yourpassword@cluster0.ayqru4p.mongodb.net/yourdbname?retryWrites=true&w=majority&appName=Cluster0/handmeabook';
-mongoose.connect(mongoURI, {
-  tls: true,
-  tlsInsecure: true // Set to false in production
-}).then(() => {
-  console.log('Successfully connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
 
-// Connect to MongoDB
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  tls: true,
-  tlsInsecure: true // Set to true for testing, false for production
-}).then(() => {
-  console.log('Successfully connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
+  useUnifiedTopology: true
 });
 
 // Export the connection
