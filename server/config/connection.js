91b3c23fab6e40004b52f3e0d40cbf97657b7acd
schrolan <schrolan000@gmail.com>
2024-07-01 21:11:49 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
 // Retrieve MongoDB URI from environment variables
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://Lance:<password>@cluster0.ayqru4p.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsInsecure=true';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://Lance:yourpassword@cluster0.ayqru4p.mongodb.net/yourdbname?retryWrites=true&w=majority&appName=Cluster0/handmeabook';
+mongoose.connect(mongoURI, {
+  tls: true,
+  tlsInsecure: true // Set to false in production
+}).then(() => {
+  console.log('Successfully connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 // Connect to MongoDB
 mongoose.connect(mongoURI, {
