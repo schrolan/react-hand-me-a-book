3c42a4a65eb0613f197ff9efbd4b5dd4bd6490eb
schrolan <schrolan000@gmail.com>
2024-07-01 21:02:09 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-//The first part after the // is the local host ip, and the 27017 is the port that mongoose runs on be default
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Lance:<password>@cluster0.ayqru4p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/handmeabook')
+// Retrieve MongoDB URI from environment variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://Lance:<password>@cluster0.ayqru4p.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsInsecure=true';
 
-//This exports the object and the connection
-module.exports = mongoose.connection
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  tlsInsecure: true // Set to true for testing, false for production
+}).then(() => {
+  console.log('Successfully connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+// Export the connection
+module.exports = mongoose.connection;
