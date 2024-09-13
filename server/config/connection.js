7adc2e297d394f538043e19d7eb6abfd41c89c98
schrolan <schrolan000@gmail.com>
2024-09-12 20:10:38 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/react-hand-me-a-book', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+module.exports = db;
