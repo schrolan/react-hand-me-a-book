289d535ae221e26452ad17f78c71d243d59c7a68
schrolan <schrolan000@gmail.com>
2024-09-12 19:56:26 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/react-hand-me-a-book')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/react-hand-me-a-book', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
