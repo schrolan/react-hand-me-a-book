fe1cff912d2212a710fed1dae8f45f6bbee92d72
Lance Schroeder <schrolan000@gmail.com>
2025-10-01 11:48:54 -0500

    Restructured the searchform

--- client/src/components/searchForm.js
+++ client/src/components/searchForm.js
@@ -1,51 +1,52 @@
 import { MdClear } from "react-icons/md"
 import Container from "./container"
 
-const SearchForm = ({ searchTerm, handleInputChange, handleFormSubmit, reset, category, handleCategoryChange }) => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <Container>
-                    <form 
-                        className="d-flex" 
-                        onSubmit={handleFormSubmit}
-                    >
-                        <input value={searchTerm}
-                            onChange={handleInputChange}
-                            placeholder="Look for a book" 
-                            className="form-control me-2" 
-                            aria-label="Search" 
-                        />
-                <select 
-                    className="btn" 
-                    id="category" 
-                    value={category} 
-                    onChange={handleCategoryChange}
-                >
-                        <option value="title">Title</option>
-                        <option value="author">Author</option>
-                        <option value="subject">Subject</option>
-                </select>
-                <button 
-                className="btn" 
-                type="submit"
-                >
-                    Search
-                </button>
+const SearchForm = ({
+  searchTerm,
+  handleInputChange,
+  handleFormSubmit,
+  reset,
+  category,
+  handleCategoryChange,
+}) => {
+  return (
+    <nav className="navbar">
+      <div className="container-fluid">
+        <Container>
+          <form className="d-flex" onSubmit={handleFormSubmit}>
+            <input
+              value={searchTerm}
+              onChange={handleInputChange}
+              placeholder="Look for a book"
+              className="form-control me-2"
+              aria-label="Search"
+            />
+
+            <select
+              className="btn"
+              id="category"
+              value={category}
+              onChange={handleCategoryChange}
+            >
+              <option value="title">Title</option>
+              <option value="author">Author</option>
+              <option value="subject">Subject</option>
+            </select>
+
+            <button className="btn" type="submit">
+              Search
+            </button>
+
             {searchTerm && (
-                <button 
-                    className="btn" 
-                    type="button" 
-                    onClick={reset}
-                >
-                    <MdClear />
-                </button>
+              <button className="btn" type="button" onClick={reset}>
+                <MdClear />
+              </button>
             )}
-                        </form>
-                </Container>
-            </div>
-        </nav>
-    )
+          </form>
+        </Container>
+      </div>
+    </nav>
+  )
 }
 
 export default SearchForm
